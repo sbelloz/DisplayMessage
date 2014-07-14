c414bf91b7bc5f5f1b63270daee35116355878d5
Simone Bellotti <simone.b93@hotmail.it>
2014-07-14 17:11:18 +0200

    bug fixes

--- DisplayMessage/src/it/bellotti/android/displaymessage/SendMessage.java
+++ DisplayMessage/src/it/bellotti/android/displaymessage/SendMessage.java
@@ -13,42 +13,36 @@ import android.widget.EditText;
 
 
 public class SendMessage extends Activity {
-	
-	private EditText mEditMessage;
-	private Button mSendButton;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_send_message);
-				
-		//Init views
-		mEditMessage = (EditText) findViewById(R.id.message);
-		mSendButton = (Button) findViewById(R.id.send);		
-	
-		mSendButton.setOnClickListener(new OnClickListener() {			
-			@Override
-			public void onClick(View v) {
-				String s = mEditMessage.getText().toString();
-				startDisplayMessage(s);
-			}
-		});
-	
-}
-	
-	private void startDisplayMessage(String s){
-		Intent i = new Intent(this, DisplayMessage.class);
-		i.putExtra(Constants.STRING_KEY, s);
-		startActivity(i);					
-	
-	}
-	
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.send_message, menu);
-		return true;
-	}
+    private EditText mEditMessage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_send_message);
+
+        //Init views
+        mEditMessage = (EditText) findViewById(R.id.message);
+    }
+
+    public void onClick(View view) {
+        String string = mEditMessage.getText().toString();
+        startDisplayMessage(string);
+    }
+
+    private void startDisplayMessage(String s) {
+        Intent i = new Intent(this, DisplayMessage.class);
+        i.putExtra(Constants.STRING_KEY, s);
+        startActivity(i);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.send_message, menu);
+        return true;
+    }
 }
 
 
